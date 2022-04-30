9f5dc86fc70743a5df2e912bebe4541a737b55aa
Igor-mt <igor.limall@outlook.com>
2022-04-30 19:51:44 -0300

    Aula 7 criação de tabela

--- application/controllers/Pagina.php
+++ application/controllers/Pagina.php
@@ -1,34 +1,41 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pagina extends CI_Controller{
+class Pagina extends CI_Controller
+{
 
-    function __construct(){
-        parent::__construct();  
-        $this->load->helper('url');      
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
     }
 
-    public function index(){
+    public function index()
+    {
         $dados['titulo'] = 'Igor Matheus Desenvolvimento Web';
         $this->load->view('home', $dados);
     }
-    
-    public function clientes(){
+
+    public function clientes()
+    {
         $dados['titulo'] = 'Clientes - Igor Matheus Desenvolvimento Web';
         $this->load->view('clientes', $dados);
     }
 
-    public function servicos(){
+    public function servicos()
+    {
         $dados['titulo'] = 'Serviços - Igor Matheus Desenvolvimento Web';
         $this->load->view('servicos', $dados);
     }
 
-    public function sobre(){
+    public function sobre()
+    {
         $dados['titulo'] = 'Sobre - Igor Matheus Desenvolvimento Web';
         $this->load->view('sobre', $dados);
     }
 
-    public function contato(){
+    public function contato()
+    {
         $this->load->helper('form');
         $this->load->library(array('form_validation', 'email'));
         //Regras de validações do formulario
@@ -36,17 +43,17 @@ class Pagina extends CI_Controller{
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
         $this->form_validation->set_rules('assunto', 'Assunto', 'trim|required');
         $this->form_validation->set_rules('mensagem', 'Mensagem', 'trim|required');
-        if($this->form_validation->run() == FALSE):
+        if ($this->form_validation->run() == FALSE) :
             $dados['formerror'] = validation_errors();
-        else:
+        else :
             $dados_form = $this->input->post();
             $this->email->from($dados_form['email'], $dados_form['nome']);
             $this->email->to('igor.limall@outlook.com');
             $this->email->subject($dados_form['assunto']);
             $this->email->message($dados_form['mensagem']);
-            if($this->email->send()):
+            if ($this->email->send()) :
                 $dados['formerror'] =  'Email enviado com sucesso!';
-            else:
+            else :
                 $dados['formerror'] = 'Erro ao enviar email, tente novamente mais tarde.';
 
             endif;
@@ -57,5 +64,3 @@ class Pagina extends CI_Controller{
         $this->load->view('contato', $dados);
     }
 }
-
-
