f26438906f7838ff41dc5514ca3e68e67cce9f7c
Igor-mt <igor.limall@outlook.com>
2022-04-30 19:51:14 -0300

    Aula 7 criação de tabela

---
+++ application/controllers/Setup.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Setup extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->load->model('Option_model', 'option');
+    }
+
+    public function index()
+    {
+        if ($this->option->get_option('setup_executado') == 1) {
+            // setup configurado, mostrar tela para editar dados de setup
+            redirect('setup/alterar', 'refresh');
+        } else {
+            // nõa instalado,, mostrar tela de alteração
+            redirect('setup/instalar', 'refresh');
+        }
+    }
+
+    public function instalar()
+    {
+        if ($this->option->get_option('setup_executado') == 1) {
+            // setup configurado, mostrar tela para editar dados de setup
+            redirect('setup/alterar', 'refresh');
+        }
+
+        // carrega view
+
+    }
+
+    public function alterar()
+    {
+        echo "metodo alterar";
+    }
+}
