46dd3062bfc6cf4fdd593e81804aa84b5c1e0694
Igor-mt <igor.limall@outlook.com>
2022-05-13 12:17:49 -0300

    Aula 8 Parte 2 criando login

--- application/helpers/funcoes_helper.php
+++ application/helpers/funcoes_helper.php
@@ -1,3 +1,22 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
+if (!function_exists('set_msg')):
+    //seta uma mensagem via session para ser lida posteriormente
+    function set_msg($msg=NULL){
+        
+        $ci = & get_instance();
+        $ci->session->set_userdata('aviso', $msg);
+    }
+endif;
+
+if (!function_exists('get_msg')):
+    //retorna uma mensagem definida pela função set-msg
+    function get_msg($destroy=TRUE)
+    {
+        $ci = & get_instance();
+        $retorno = $ci->session->userdata('aviso');
+        if ($destroy) $ci->session->unset_userdata('aviso');
+        return $retorno;
+    }
+endif;
