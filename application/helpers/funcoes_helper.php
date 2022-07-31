8ace2b0254890227175f03be9e885da8a40f70a0
Igor-mt <igor.limall@outlook.com>
2022-07-31 20:58:11 -0300

    New Login Page

--- application/helpers/funcoes_helper.php
+++ application/helpers/funcoes_helper.php
@@ -1,43 +1,54 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-if (!function_exists('set_msg')):
+if (!function_exists('getTemplate')) :
     //seta uma mensagem via session para ser lida posteriormente
-    function set_msg($msg=NULL){
-        
-        $ci = & get_instance();
+    function getTemplate()
+    {
+        return base_url("assets/sbadmin2");
+    }
+endif;
+
+if (!function_exists('set_msg')) :
+    //seta uma mensagem via session para ser lida posteriormente
+    function set_msg($msg = NULL)
+    {
+
+        $ci = &get_instance();
         $ci->session->set_userdata('aviso', $msg);
     }
 endif;
 
-if (!function_exists('get_msg')):
+if (!function_exists('get_msg')) :
     //retorna uma mensagem definida pela função set-msg
-    function get_msg($destroy=TRUE)
+    function get_msg($destroy = TRUE)
     {
-        $ci = & get_instance();
+        $ci = &get_instance();
         $retorno = $ci->session->userdata('aviso');
         if ($destroy) $ci->session->unset_userdata('aviso');
         return $retorno;
     }
 endif;
 
-if(!function_exists('verifica_login')):
+if (!function_exists('verifica_login')) :
     //verifica se o usuário está logado, caso negativa redireciona para outra página
-    function verifica_login($redirect='setup/login'){
-        $ci = & get_instance();
-        if($ci->session->userdata('logged') != TRUE):
+    function verifica_login($redirect = 'setup/login')
+    {
+        $ci = &get_instance();
+        if ($ci->session->userdata('logged') != TRUE) :
             set_msg('<p>Acesso restrito! Faça o login para continuar.</p>');
             redirect($redirect, 'reflesh');
         endif;
     }
 endif;
 
-if(!function_exists('config_upload')):
+if (!function_exists('config_upload')) :
     // define as configuações para upload de imagem/arquivos
-    function config_upload($path='./uploads', $types='jpg|png', $size=512){
+    function config_upload($path = './uploads', $types = 'jpg|png', $size = 512)
+    {
         $config['upload_path'] = $path;
         $config['allowed_types'] = $types;
         $config['max_size'] = $size;
         return $config;
- }
+    }
 endif;
--- application/views/painel/login.php
+++ application/views/painel/login.php
@@ -1,35 +1,91 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <title><?php echo $titulo; ?></title>
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/normalize.css') ?>" />
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700' rel='stylesheet' type='text/css' />
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/estilo.css') ?>" />
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/painel.css') ?>" />
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="<?php echo getTemplate(); ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?php echo getTemplate(); ?>/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
-<body>
-    <div class="linha">
-        <div class="coluna col4">&nbsp;</div>
-        <div class="coluna col4 login">
-        <h2><?php echo $h2; ?></h2>
-        <?php
-        if($msg = get_msg()):
-            echo '<div class="msg-box">' .$msg. '</div>';
-        endif;
-        echo form_open();
-        echo form_label('Usuário:','login');
-        echo form_input('login', set_value('login'), array('autofocus' => 'autofocus'));
-        echo form_label('Senha', 'senha');
-        echo form_password('senha');
-        echo form_submit('enviar', 'Autenticar', array('class' => 'botao'));
-        echo form_close();
-        ?>
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4"><?php echo $h2; ?></h1>
+                                    </div>
+
+                                    <div class="linha">
+                                        <div class="coluna col4">&nbsp;</div>
+                                        <div class="coluna col4 login">
+                                            <?php
+                                            if ($msg = get_msg()) :
+                                                echo '<div class="msg-box">' . $msg . '</div>';
+                                            endif;
+                                            echo form_open();
+                                            echo form_input('login', set_value('login'), array('autofocus' => 'autofocus', 'class' => 'form-control-user form-control'));
+                                            echo "<br>";
+                                            echo form_password('senha', '', array('class' => 'form-control-user form-control'));
+                                            echo "<br>";
+                                            echo form_submit('enviar', 'Autenticar', array('class' => 'btn btn-primary btn-user btn-block'));
+                                            echo form_close();
+                                            ?>
+                                        </div>
+                                        <div class="coluna col4">&nbsp;</div>
+                                    </div>
+
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Esqueceu a senha?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Criar uma conta!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
-        <div class="coluna col4">&nbsp;</div>
-    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?php echo getTemplate(); ?>/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo getTemplate(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?php echo getTemplate(); ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?php echo getTemplate(); ?>/js/sb-admin-2.min.js"></script>
+
 </body>
 
 </html>
